fcf47dcac960b04615ad0ece93b221c1b4187af0
Vinicius Reis <vinicius.moreira2003@gmail.com>
2021-01-02 02:04:52 -0300

    Style and creating container

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles'
+
+function App() {
+  return (
+    
+    <>
+    <h1>Twitter</h1>
+
+    <GlobalStyles/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from '../Main';
+import { Container, Wrapper } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+      <Container>
+          <Wrapper>
+            {/* <MenuBar></MenuBar> */}
+            <Main></Main>
+            {/* <SideBar></SideBar> */}
+          </Wrapper>
+      </Container>
+  );
+}
+
+export default Layout;
